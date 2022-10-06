d8000e36649228c2856c7fbaa54aad750e4109dc
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-06 22:02:46 +0000

    change project_root to LAUREL

--- LAUREL/asgi.py
+++ LAUREL/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project_root project.
+ASGI config for LAUREL project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_root.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LAUREL.settings')
 
 application = get_asgi_application()
